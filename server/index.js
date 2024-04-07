f5166256786be64579a8a7bdf1b153f9993c77ea
Rahul_kottak <114240026+Rahulkottak@users.noreply.github.com>
2024-04-07 16:01:23 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -35,14 +35,14 @@ app.use(
 	})
 );
 
-// app.use(function(req, res, next) {
-// 	res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-// 	res.header(
-// 	  'Access-Control-Allow-Headers',
-// 	  'Origin, X-Requested-With, Content-Type, Accept'
-// 	);
-// 	next();
-// });
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', 'https://studynotion-edtech-platform-zeta.vercel.app');
+	res.header(
+	  'Access-Control-Allow-Headers',
+	  'Origin, X-Requested-With, Content-Type, Accept'
+	);
+	next();
+});
 
 app.use(
 	fileUpload({
