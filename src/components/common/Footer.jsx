5d3ae600edffe83c5b314ad2073f23d4b42b57e9
r@hul <114240026+coderhuBypassion@users.noreply.github.com>
2024-09-28 16:38:13 +0530

    Update Footer.jsx

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -172,7 +172,7 @@ const Footer = () => {
             })}
           </div>
 
-          <div className="text-center">Made with ❤️ by Basant Agrawal © 2024 Studynotion</div>
+          <div className="text-center">Made with ❤️ by Rahul kottak © 2024 Studynotion</div>
         </div>
       </div>
     </div>
